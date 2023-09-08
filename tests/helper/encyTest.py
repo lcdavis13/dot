76211244e7ce6964c77536340936595abac49ad1
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-09-08 11:55:31 -1000

    testing updates

--- tests/helper/encyTest.py
+++ tests/helper/encyTest.py
@@ -11,7 +11,44 @@ class encyTest(ency):
 
         super().__init__(("dot1", "dot2", "dot3"))
 
+    def selfTest(self, ut):
+        '''Note that this test is self-destructive, it only works once'''
+        ut.assertEqual(self.f, {'x': 0.6})
+    
+        self.f["x"] = 0.7
+        ut.assertEqual(self.f, {'x': 0.7})
+    
+        del self.f
+        ut.assertEqual(self.f, {'y': 6})
+        ut.assertEqual(self.f.y, 6)
+    
+        self.f.y = 7
+        ut.assertEqual(self.f, {'y': 7})
+        ut.assertEqual(self.f.y, 7)
+    
+        del self.f
+        ut.assertEqual(self.f, {'z': 66})
+    
+        self.f["z"] = 77
+        ut.assertEqual(self.f, {'z': 77})
+    
+        del self.f
+        with ut.assertRaises(AttributeError):
+            self.f
+
 class encyTestSimple(ency):
     def __init__(self):
         self.dot1 = {"a": 1, "b": 2}
         super().__init__(("dot1"))
+
+    def selfTest(self, ut):
+        ut.assertEqual(self.f, {'a': 1})
+    
+        ut.assertEqual(self.f, {'b': 2})
+
+        self.f.c = 3
+        ut.assertEqual(self.f, {'c': 3})
+
+        del self.f.c
+        with ut.assertRaises(AttributeError):
+            self.f
--- tests/test_ency.py
+++ tests/test_ency.py
@@ -4,44 +4,11 @@ from dot.dicy import dicy
 from dot.ency import ency
 from .helper.encyTest import encyTest, encyTestSimple
 
-
 class test_ency(unittest.TestCase):
     def test(self):
-        
-        class encyTest(ency):
-            def __init__(self):
-                self.dot1 = {"a": 1, "b": 2}
-                self.dot2 = dicy({"c": 3, "e": 5, "f": dicy({"y": 6})})
-                self.dot3 = {"d": 4, "e": 55, "f": {"z": 66}}
-                self.e = 0.5
-                self.f = {"x": 0.6}
-                
-                super().__init__(("dot1", "dot2", "dot3"))
-        
         ef = encyTest()
+        ef.selfTest(self)
         
-        self.assertEqual(ef.f, {'x': 0.6})
-        
-        ef.f["x"] = 0.7
-        self.assertEqual(ef.f, {'x': 0.7})
-        
-        del ef.f
-        self.assertEqual(ef.f, {'y': 6})
-        self.assertEqual(ef.f.y, 6)
-        
-        ef.f.y = 7
-        self.assertEqual(ef.f, {'y': 7})
-        self.assertEqual(ef.f.y, 7)
-        
-        del ef.f
-        self.assertEqual(ef.f, {'z': 66})
-        
-        ef.f["z"] = 77
-        self.assertEqual(ef.f, {'z': 77})
-        
-        del ef.f
-        with self.assertRaises(AttributeError):
-            ef.f
             
     def test_readwriteSimple(self):
 
@@ -60,14 +27,8 @@ class test_ency(unittest.TestCase):
             js = pickle.dumps(ef)
             ef2 = pickle.loads(js)
 
-            self.assertEqual(ef2.dot1["a"], ef.dot1["a"])
-            self.assertEqual(ef2.dot1["b"], ef.dot1["b"])
-            self.assertEqual(ef2.dot2["c"], ef.dot2["c"])
-            self.assertEqual(ef2.dot2["e"], ef.dot2["e"])
-            self.assertEqual(ef2.dot2["f"]["y"], ef.dot2["f"]["y"])
-            self.assertEqual(ef2.dot3["d"], ef.dot3["d"])
-            self.assertEqual(ef2.dot3["e"], ef.dot3["e"])
-            self.assertEqual(ef2.dot3["f"]["z"], ef.dot3["f"]["z"])
+            ef.selfTest(self)
+            ef2.selfTest(self)
 
 
 if __name__ == '__main__':

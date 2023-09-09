2fd2a40cf6a6defd6e583f4ec2e22448aabb5ab2
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2023-09-08 14:07:13 -1000

    ency unit test expansion

--- tests/helper/encyTest.py
+++ tests/helper/encyTest.py
@@ -13,26 +13,116 @@ class encyTest(ency):
 
     def selfTest(self, ut):
         '''Note that this test is self-destructive, it only works once'''
-        ut.assertEqual(self.f, {'x': 0.6})
+        
+        #original state test
+        ut.assertEqual(self.a, 1)
+        ut.assertEqual(self.b, 2)
+        ut.assertEqual(self.c, 3)
+        ut.assertEqual(self.d, 4)
+        ut.assertEqual(self.e, 0.5)
+        ut.assertEqual(self.f, {"x":0.6})
+        ut.assertEqual(self.f["x"], 0.6)
+        ut.assertEqual(self.dot2.e, 5)
+        ut.assertEqual(self.dot2.f, {"y":6})
+        ut.assertEqual(self.dot2.f.y, 6)
+        ut.assertEqual(self.dot3["e"], 55)
+        ut.assertEqual(self.dot3["f"], {"z":66})
+        ut.assertEqual(self.dot3["f"]["z"], 66)
+        
+        
+        #INSERTION TEST
+        
+        #insert to class by default
+        self.w = 0.7
+        ut.assertEqual(self.w, 0.7)
+        ut.assertEqual(self.__dict__["w"], 0.7)
+        with ut.assertRaises(KeyError):
+            self.dot1["w"]
+        with ut.assertRaises(AttributeError):
+            self.dot2.w
+        with ut.assertRaises(KeyError):
+            self.dot3["w"]
+        #delete
+        del self.w
+        with ut.assertRaises(AttributeError):
+            self.w
+
+        # insert to first dict
+        self.dot1["w"] = 7
+        ut.assertEqual(self.w, 7)
+        with ut.assertRaises(KeyError):
+            self.__dict__["w"]
+        with ut.assertRaises(AttributeError):
+            self.dot2.w
+        with ut.assertRaises(KeyError):
+            self.dot3["w"]
+        #delete
+        del self.w
+        with ut.assertRaises(AttributeError):
+            self.w
+
+        # insert to second dict (a dicy)
+        self.dot2.w = 70
+        ut.assertEqual(self.w, 70)
+        with ut.assertRaises(KeyError):
+            self.__dict__["w"]
+        with ut.assertRaises(KeyError):
+            self.dot1["w"]
+        with ut.assertRaises(KeyError):
+            self.dot3["w"]
+        #delete
+        del self.w
+        with ut.assertRaises(AttributeError):
+            self.w
+
+        # insert to third dict
+        self.dot3["w"] = 700
+        ut.assertEqual(self.w, 700)
+        with ut.assertRaises(KeyError):
+            self.__dict__["w"]
+        with ut.assertRaises(KeyError):
+            self.dot1["w"]
+        with ut.assertRaises(AttributeError):
+            self.dot2.w
+        #delete
+        del self.w
+        with ut.assertRaises(AttributeError):
+            self.w
+            
+        
+        #TODO thorough reassignment tests
     
+    
+        #OVERLAPPING KEYS TESTING
+        
+        #class member
+        ut.assertEqual(self.f, {"x":0.6})
+        ut.assertEqual(self.f["x"], 0.6)
+        # reassignment
         self.f["x"] = 0.7
         ut.assertEqual(self.f, {'x': 0.7})
-    
+        # deletion
         del self.f
+        
+        #second dict (highest priority that has the key)
         ut.assertEqual(self.f, {'y': 6})
         ut.assertEqual(self.f.y, 6)
-    
+        # reassignment
         self.f.y = 7
         ut.assertEqual(self.f, {'y': 7})
         ut.assertEqual(self.f.y, 7)
-    
+        # deletion
         del self.f
+        
+        #third dict (lowest priority)
         ut.assertEqual(self.f, {'z': 66})
-    
+        # reassignment
         self.f["z"] = 77
         ut.assertEqual(self.f, {'z': 77})
-    
+        #deletion
         del self.f
+        
+        #key is now fully gone
         with ut.assertRaises(AttributeError):
             self.f
 

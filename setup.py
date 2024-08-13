619631efb94cef0a1209740e0eceea33f3fd7f0f
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2024-08-13 10:55:46 -1000

    changed PyPI project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dot", 
+    name="dotsy", 
     version="0.1.0",  
     author="Levi Davis",
     author_email="levi.davis.co@gmail.com",
